0a892c7dc2ad6e855d8c585ba79da170aef68120
Palak Gupta <76204311+Palakgupta0908@users.noreply.github.com>
2021-10-05 16:05:20 +0530

    Initial files

---
+++ questions.js
@@ -0,0 +1,70 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Who invented JavaScript?",
+    answer: "Brendan Eich",
+    options: [
+      "Douglas Crockford",
+      "Brendan Eich",
+      "Sheryl Sandberg",
+      "James Gosling"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "VPN stands for?",
+    answer: "Virtual Private Network",
+    options: [
+      "Virtual Private Network",
+      "Virtual Path Network",
+      "Virtual Planning Network",
+      "Virtual Public Network"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "The <hr> tag in HTML is used for?",
+    answer: "Yet Another Markup Language",
+    options: [
+      "new paragraph",
+      "new line",
+      "vertical ruler",
+      "horizontal ruler"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
